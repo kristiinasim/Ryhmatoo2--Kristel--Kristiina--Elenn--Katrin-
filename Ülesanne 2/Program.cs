69bec841e0b47863133a0bbcf1f3287468d75620
kristiinasim <kristiina.simulman@tptlive.ee>
2017-11-23 09:56:19 +0200

    Ülesanne2 praegu on vale, vaja muuta

---
+++ Ülesanne 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+namespace Ülesanne_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = (@"C:\Users\opilane\Documents\GitHub\Ryhmatoo2--Kristel--Kristiina--Elenn--Katrin-\kttekst").Count();
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
