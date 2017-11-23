8b3182a2e0ab7021f5abdd105ebedb4bae4ba763
KristelEiTea <kristel.siilak@hotmail.com>
2017-11-23 09:40:06 +0200

    Alus

---
+++ Ryhmat88/Ryhmat88/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryhmat88
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
