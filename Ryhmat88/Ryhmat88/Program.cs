5f382949ccad1b94b79bcfaee610acc12d05cc9c
KristelEiTea <kristel.siilak@hotmail.com>
2017-11-26 17:17:29 +0200

    rjkhgkwrhg

--- Ryhmat88/Ryhmat88/Program.cs
+++ Ryhmat88/Ryhmat88/Program.cs
@@ -11,6 +11,31 @@ namespace Ryhmat88
         static void Main(string[] args)
         {
 
+            // 1) Kas listides leidub sarnaseid elemente (ühisosa)
+             //var list = new List<string>{ "1", "2", "36", "3", "56", "2" };
+            var q = from s in list
+                    group s by s into g
+                    where g.Count() > 1
+                    select g.First();
+
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+
+            }
+            //4) Leidge mõlemal listil eraldi numbrite keskmine
+           
+             //var list = new List<string> { "1", "2", "36", "3", "56", "2" };  
+             //var list2 = new List<string> { "11", "2", "36", "73", "56", "2" };
+
+            double result = (from x in list select x.Length).Average();
+            Console.WriteLine(result);
+
+            double result2 = (from x in list2 select x.Length).Average();
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+
             //Ülesanne 3:
             Console.WriteLine("3. 2)");
             Console.WriteLine("Kahe listi suurim number on:");
@@ -23,6 +48,7 @@ namespace Ryhmat88
             Console.WriteLine("3. 5)");
             Console.WriteLine("Kahe list numbrite keskmine koos on:");
             Console.WriteLine(YlesanneKolm.Mkeskmine);
+
         }
     }
 }
