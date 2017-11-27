6468529ae3a0f7ee133ba106d52ad15ef36d376c
triinutamm <triinu.tamm.1995@gmai.com>
2017-11-28 01:12:29 +0200

    ül1

---
+++ Ryhmat88/Ryhmat88/Ülesanne1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryhmat88
+{
+    class ülesanne1 : Program
+    {
+        public static int Ajunuss(int arv1, int arv2)
+        {
+            if (arv1 < arv2)
+            {
+                while (arv1 < arv2 - 1)
+                {
+                    arv1++;
+                    if (arv1 % 2 == 0)
+                    {
+                        Console.WriteLine(arv1);
+                    }
+
+                }
+            }
+            else
+            {
+                while (arv2 < arv1 - 1)
+                {
+                    arv2++;
+                    if (arv2 % 2 == 0)
+                    {
+                        Console.WriteLine(arv2);
+                    }
+                }
+
+            }
+            return 0;
+        }
+    }
+}
